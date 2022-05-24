b8f1d249113a8763f1004c9888c9b26f54a0bd3f
AndySast <andressastoquexyz@gmail.com>
2022-05-24 08:21:52 -0500

    CardDetail

--- src/Components/Pages/ProductDetail/ProductDetail.js
+++ src/Components/Pages/ProductDetail/ProductDetail.js
@@ -1,16 +1,7 @@
+import CardDetail from '../../CardDetail';
+
 function ProductDetail() {
-  return (
-    <div className="ProductDetail">
-      <h1>ProductDetail</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-        eget nisl euismod, aliquam erat eu, consectetur eros. Donec eget nisl
-        euismod, aliquam erat eu, consectetur eros. Donec eget nisl euismod,
-        aliquam erat eu, consectetur eros. Donec eget nisl euismod, aliquam erat
-        eu,
-      </p>
-    </div>
-  );
+  return <CardDetail />;
 }
 
 export default ProductDetail;
